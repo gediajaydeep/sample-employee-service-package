21f5e9bdf780da2e6bfe6c773ba6b77a4ef77770
gediajaydeep <gediajaydeep@gmail.com>
2026-01-07 17:50:55 +0530

    Country Tests - Create - Failed case

--- employee_service/test/employee_service_test.dart
+++ employee_service/test/employee_service_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:employee_service/employee_service.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+test('Country model should be initializable with name and tax rate', () {
+    final country = Country(name: 'India', taxRate: 0.10);
+    
+    expect(country.name, 'India');
+    expect(country.taxRate, 0.10);
   });
 }
