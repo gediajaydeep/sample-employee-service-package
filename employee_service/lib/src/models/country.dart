5bfba3daa49484396ce3273d5c1f8d81b47498c3
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 11:50:22 +0530

    country model serialization : passed test

--- employee_service/lib/src/models/country.dart
+++ employee_service/lib/src/models/country.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'country.g.dart'; 
+
+@JsonSerializable(fieldRename: FieldRename.snake)
 class Country {
   final int? id;
   final String name;
@@ -8,4 +13,7 @@ class Country {
     required this.name,
     required this.taxRate,
   });
+
+  factory Country.fromJson(Map<String, dynamic> json) => _$CountryFromJson(json);
+  Map<String, dynamic> toJson() => _$CountryToJson(this);
 }
---
+++ employee_service/lib/src/models/country.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'country.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Country _$CountryFromJson(Map<String, dynamic> json) => Country(
+  id: (json['id'] as num?)?.toInt(),
+  name: json['name'] as String,
+  taxRate: (json['tax_rate'] as num).toDouble(),
+);
+
+Map<String, dynamic> _$CountryToJson(Country instance) => <String, dynamic>{
+  'id': instance.id,
+  'name': instance.name,
+  'tax_rate': instance.taxRate,
+};
