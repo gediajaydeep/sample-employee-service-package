1ce75acb14d66650235d22105993ecb30ecad785
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 19:42:38 +0530

    EmployeeFilters inital : Failed Case

---
+++ employee_service/test/src/models/employee_filter_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Initial state should be empty', () {
+    final filter = EmployeeFilter();
+    expect(filter.isEmpty, isTrue);
+  });
+}
