58d328986cbae8e0975177547f829c95b683f596
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 12:35:23 +0530

    Employee Model : Serialization : Failed cases

--- employee_service/test/src/models/employee_test.dart
+++ employee_service/test/src/models/employee_test.dart
@@ -1,6 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:employee_service/src/models/country.dart';
 import 'package:employee_service/src/models/employee.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -22,5 +22,32 @@ void main() {
     expect(employee.country?.id, 1);
     expect(employee.country?.name, 'India');
     expect(employee.country?.taxRate, 0.10);
+  });
+
+  test('Employee model should convert fromJson or toJson correctly', () {
+    final json =
+        {
+              'id': 1,
+              'full_name': 'Jaydeep Gedia',
+              'job_title': 'Software Engineer',
+              'salary': 50000.0,
+              'country_id': 1,
+              'country': {'id': 1, 'name': 'India', 'tax_rate': 0.10},
+            }
+            as Map<String, dynamic>;
+
+    final employee = Employee.fromJson(json);
+
+    expect(employee.id, json['id']);
+    expect(employee.fullName, json['full_name']);
+    expect(employee.jobTitle, json['job_title']);
+    expect(employee.salary, json['salary']);
+    expect(employee.countryId, json['country_id']);
+
+    expect(employee.country?.id, json['country']['id']);
+    expect(employee.country?.name, json['country']['name']);
+    expect(employee.country?.taxRate, json['country']['tax_rate']);
+
+    expect(DeepCollectionEquality().equals(employee.toJson(), json), isTrue);
   });
 }
