ea2c199578a953f16caf9439a81986313e8ed20f
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 17:31:09 +0530

    CountryRepo : getById : Failed Cases

--- employee_service/test/src/repositories/local_country_repository_test.dart
+++ employee_service/test/src/repositories/local_country_repository_test.dart
@@ -30,4 +30,32 @@ main() {
       ).called(1);
     },
   );
+
+  test(
+    'getById() should use db.query return Country object when record exists',
+    () async {
+      final mockRow = {'id': 1, 'name': 'USA', 'tax_rate': 0.12};
+      when(() => mockDb.query(any(), any())).thenAnswer((_) async => [mockRow]);
+
+      final result = await repository.getById(1);
+
+      expect(result, isA<Country>());
+      expect(result?.name, 'USA');
+      verify(
+        () => mockDb.query(any(that: contains('WHERE id = ?')), [1]),
+      ).called(1);
+    },
+  );
+
+  test(
+    'getById() should use db.query and throw an exception if record does not exist',
+    () async {
+      when(() => mockDb.query(any(), any())).thenThrow(Exception());
+
+      expect(repository.getById(1), throwsException);
+      verify(
+        () => mockDb.query(any(that: contains('WHERE id = ?')), [1]),
+      ).called(1);
+    },
+  );
 }
