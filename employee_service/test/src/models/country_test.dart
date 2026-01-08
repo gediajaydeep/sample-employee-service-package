076ff3c14e4cf0979de59c75adcb3915ba3d5a12
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 11:44:28 +0530

    Country serializatin failed case

--- employee_service/test/src/models/country_test.dart
+++ employee_service/test/src/models/country_test.dart
@@ -1,4 +1,5 @@
 import 'package:employee_service/src/models/country.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -7,5 +8,20 @@ void main() {
     
     expect(country.name, 'India');
     expect(country.taxRate, 0.10);
+  });
+
+  test('Country model should convert to/from JSON correctly',
+  (){
+    final json = {
+      'name' : 'India',
+      'tax_rate' : 0.10
+    };
+    final country = Country.fromJson(json);
+    expect(country.name, equals(json['name']));
+    expect(country.taxRate, equals(json['tax_rate']));
+
+    expect(mapEquals(country.toJson(), json), isTrue) ;
+
+
   });
 }
