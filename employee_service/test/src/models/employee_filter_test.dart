ce13543acd51a677bc2100ac8e6b30826af1d08a
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 19:55:27 +0530

    EmployeeFilter : byCountryID :Failed Case

--- employee_service/test/src/models/employee_filter_test.dart
+++ employee_service/test/src/models/employee_filter_test.dart
@@ -8,17 +8,28 @@ void main() {
   });
 
   test('byCountryId() should add a correct QueryCondition', () {
-      final filter = EmployeeFilter();
-      const countryId = 5;
+    final filter = EmployeeFilter();
+    const countryId = 5;
 
-      filter.byCountryId(countryId);
+    filter.byCountryId(countryId);
 
-      expect(filter.isEmpty, isFalse);
-      expect(filter.conditions.length, 1);
-      
-      final condition = filter.conditions.first;
-      expect(condition.field, 'country_id');
-      expect(condition.operator, QueryOperator.equals);
-      expect(condition.value, countryId);
-    });
+    expect(filter.isEmpty, isFalse);
+    expect(filter.conditions.length, 1);
+
+    final condition = filter.conditions.first;
+    expect(condition.field, 'country_id');
+    expect(condition.operator, QueryOperator.equals);
+    expect(condition.value, countryId);
+  });
+
+  test('byJobTitle() should add a correct QueryCondition', () {
+    final filter = EmployeeFilter();
+    const title = 'Developer';
+
+    filter.byJobTitle(title);
+
+    final condition = filter.conditions.first;
+    expect(condition.field, 'job_title');
+    expect(condition.value, title);
+  });
 }
