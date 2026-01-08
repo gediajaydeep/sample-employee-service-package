720e246a21bb0bc71c8106a1c5a715cc81875867
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 15:43:07 +0530

    SQFLIteDBHelper : add fields : Failed Case

--- employee_service/test/src/database/sqflite_database_helper_test.dart
+++ employee_service/test/src/database/sqflite_database_helper_test.dart
@@ -1,10 +1,14 @@
 import 'package:employee_service/src/database/sqflite_database_helper.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-
-test('SQFLiteDBHelper is created correctly',() {
-  final SqliteDatabaseHelper dbHelper = SqliteDatabaseHelper();
-},);
+  const dbName = 'employee_management.db';
+  test('SQFLiteDBHelper is created correctly', () {
+    final SqliteDatabaseHelper dbHelper = SqliteDatabaseHelper(
+      databasePath: dbName,
+      onCreate: (db, version) async {
+        
+      },
+    );
+  });
 }
