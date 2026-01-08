f7c335c2ac322d26610afc220e724ffd8484d467
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 20:14:34 +0530

    SalaryMetrics : Serialization : Failed Case

--- employee_service/test/src/models/salary_metrics_test.dart
+++ employee_service/test/src/models/salary_metrics_test.dart
@@ -2,6 +2,12 @@ import 'package:employee_service/src/models/salary_metrics.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  final mockDbResult = {
+    'average_salary': 55000.50,
+    'min_salary': 30000.0,
+    'max_salary': 95000.0,
+    'total_employees': 12,
+  };
   test('Should create SalaryMetrics instance via constructor', () {
     final metrics = SalaryMetrics(
       averageSalary: 55000.50,
@@ -14,4 +20,16 @@ void main() {
     expect(metrics.maxSalary, 90000.0);
     expect(metrics.totalEmployees, 15);
   });
+
+  test(
+    'fromJson should populate every field correctly from snake_case input',
+    () {
+      final metrics = SalaryMetrics.fromJson(mockDbResult);
+
+      expect(metrics.averageSalary, 55000.50);
+      expect(metrics.minSalary, 30000.0);
+      expect(metrics.maxSalary, 95000.0);
+      expect(metrics.totalEmployees, 12);
+    },
+  );
 }
