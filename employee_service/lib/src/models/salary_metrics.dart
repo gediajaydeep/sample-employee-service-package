2364ab1919c444de7b9b5b888acd5644bc3fbd9d
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 20:05:21 +0530

    SalaryMatrics : creation : Passed Case

---
+++ employee_service/lib/src/models/salary_metrics.dart
@@ -0,0 +1,13 @@
+class SalaryMetrics {
+  final double averageSalary;
+  final double minSalary;
+  final double maxSalary;
+  final int totalEmployees;
+
+  SalaryMetrics({
+    required this.averageSalary,
+    required this.minSalary,
+    required this.maxSalary,
+    required this.totalEmployees,
+  });
+}
