373d7852a919aac366171333d2e856499eccb33c
gediajaydeep <gediajaydeep@gmail.com>
2026-01-08 19:51:25 +0530

    EmployeeFilter : byCountryId : Passed case

--- employee_service/lib/src/models/employee_filter.dart
+++ employee_service/lib/src/models/employee_filter.dart
@@ -1,7 +1,4 @@
-
-enum QueryOperator {
-  equals,
-}
+enum QueryOperator { equals }
 
 class QueryCondition {
   final String field;
@@ -19,6 +16,16 @@ class EmployeeFilter {
   final List<QueryCondition> _conditions = [];
 
   List<QueryCondition> get conditions => List.unmodifiable(_conditions);
+
+  void byCountryId(int id) {
+    _conditions.add(
+      QueryCondition(
+        field: 'country_id',
+        operator: QueryOperator.equals,
+        value: id,
+      ),
+    );
+  }
 
   bool get isEmpty => _conditions.isEmpty;
 }
